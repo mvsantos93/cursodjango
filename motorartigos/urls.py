6d4183714c77b7ee7b2680b1ed6f3e93849f95a4
Marcos(django) <marcosvinicius2610@protonmail.com>
2026-06-18 17:55:52 -0300

    Aula 5

--- motorartigos/urls.py
+++ motorartigos/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from motorartigos.views import index
+from motorartigos.views import index, artigo
 
 urlpatterns = [
     path('', index),
+    path('artigo/', artigo, name='artigo'),
 ]
--- motorartigos/views.py
+++ motorartigos/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request,'motorartigos/index.html')
+
+def artigo(request):
+    return render(request, 'motorartigos/artigo.html')
