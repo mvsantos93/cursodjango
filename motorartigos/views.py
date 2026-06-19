8e1602a674ee9dd1a43a1f932c83d24ca7cec261
Marcos(django) <marcosvinicius2610@protonmail.com>
2026-06-19 17:48:01 -0300

    Aula 06

--- motorartigos/views.py
+++ motorartigos/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from motorartigos.models import Autor
 
 def index(request):
-    return render(request,'motorartigos/index.html')
+    """autores = {
+        1: {'nome': "Marcos",
+            'biografia': "Game Designer",
+            'email': "mvsantos@email.com"},
+
+        2: {'nome': "Kesia",
+            'biografia': "Pintora",
+            'email': "kesia@email.com"},
+
+        3: {'nome': "Samantha",
+            'biografia': 'Filha de Marcos e Kesia',
+            'email': "samantha@email.com"},
+            }
+    """
+    autores = Autor.objects.all()
+    
+    return render(request,'motorartigos/index.html', {"autores":autores})
+
 
 def artigo(request):
     return render(request, 'motorartigos/artigo.html')
