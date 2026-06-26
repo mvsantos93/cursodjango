d5048a51722f9a3cd2f1f6d0ec24ee8f2cc8e22f
Marcos(django) <marcosvinicius2610@protonmail.com>
2026-06-26 16:05:31 -0300

    Aula 08 revisado

--- motorartigos/models.py
+++ motorartigos/models.py
@@ -1,45 +1,77 @@
 from django.db import models
+from tinymce.models import HTMLField
 
-# Create your models here.
-# aqui vou criar minhas classses de entidade
 
 class Autor(models.Model):
-    #O atributo id é automático.
-    # chave primária: imutável, universal e única
-    nome = models.CharField(max_length=100)
-    biografia = models.TextField()
-    email = models.EmailField()
+    nome = models.CharField(max_length=100, verbose_name='Nome')
+    biografia = models.TextField(verbose_name='Biografia')
+    email = models.EmailField(verbose_name='E-mail')
 
     def __str__(self):
         return self.nome
+
     class Meta:
         db_table = 'autor'
+        verbose_name = 'Autor'
+        verbose_name_plural = 'Autores'
 
 
 class EixoTecnologia(models.Model):
-    nome = models.CharField(max_length=60)
+    nome = models.CharField(max_length=60, verbose_name='Nome')
 
     def __str__(self):
         return self.nome
+
     class Meta:
         db_table = 'eixo'
+        verbose_name = 'Eixo de Tecnologia'
+        verbose_name_plural = 'Eixos de Tecnologia'
+
 
 class Artigo(models.Model):
-    texto = models.TextField()
-    data_publicacao = models.DateTimeField(auto_now_add=True)
+
+    titulo = models.CharField(        
+        max_length=200,
+        verbose_name='Título',
+        default='Sem título'
+    )
+    TAG_NIVEL = [
+        ("B", "Básico"),
+        ("I", "Intermediário"),
+        ("A", "Avançado"),
+    ]
+
+    nivel = models.CharField(
+        max_length=1,
+        choices=TAG_NIVEL,
+        default='B',
+        verbose_name='Nível'
+    )
+    texto = HTMLField(verbose_name='Texto')
+    data_publicacao = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Data de Publicação'
+    )
     id_fk_eixo = models.ForeignKey(
         EixoTecnologia,
         on_delete=models.CASCADE,
-        db_column='id_fk_eixo'
+        db_column='id_fk_eixo',
+        verbose_name='Eixo',        
+        related_name='artigos'
     )
     id_fk_autor = models.ForeignKey(
         Autor,
         on_delete=models.CASCADE,
-        db_column='id_fk_autor'
+        db_column='id_fk_autor',
+        verbose_name='Autor',       
+        related_name='artigos'
     )
 
     def __str__(self):
-        return f"Artigo {self.id} - {self.data_publicação}"
-    
+        return f"Artigo {self.id} – {self.id_fk_autor.nome}"
+
     class Meta:
         db_table = 'artigo'
+        verbose_name = 'Artigo'
+        verbose_name_plural = 'Artigos'
+        ordering = ['-data_publicacao']
--- motorartigos/views.py
+++ motorartigos/views.py
@@ -2,24 +2,9 @@ from django.shortcuts import render
 from motorartigos.models import Autor
 
 def index(request):
-    """autores = {
-        1: {'nome': "Marcos",
-            'biografia': "Game Designer",
-            'email': "mvsantos@email.com"},
-
-        2: {'nome': "Kesia",
-            'biografia': "Pintora",
-            'email': "kesia@email.com"},
-
-        3: {'nome': "Samantha",
-            'biografia': 'Filha de Marcos e Kesia',
-            'email': "samantha@email.com"},
-            }
-    """
     autores = Autor.objects.all()
-    
-    return render(request,'motorartigos/index.html', {"autores":autores})
-
+   
+    return render(request,'motorartigos/index.html')
 
 def artigo(request):
-    return render(request, 'motorartigos/artigo.html')
+    return render(request,'motorartigos/artigo.html')
