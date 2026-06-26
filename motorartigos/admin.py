05ea50ca22583e792088bece86d49b8e5fbe7c38
Marcos(django) <marcosvinicius2610@protonmail.com>
2026-06-26 15:01:17 -0300

    Aula 08

--- motorartigos/admin.py
+++ motorartigos/admin.py
@@ -2,26 +2,38 @@ from django.contrib import admin
 from tinymce.widgets import TinyMCE
 from django.db import models as db_models
 from .models import Autor, EixoTecnologia, Artigo
-
+from html.parser import HTMLParser
+from bs4 import BeautifulSoup
 
 @admin.register(Autor)
 class AutorAdmin(admin.ModelAdmin):
     list_display = ('id', 'nome', 'email')
     search_fields = ('nome', 'email')
 
-
 @admin.register(EixoTecnologia)
 class EixoTecnologiaAdmin(admin.ModelAdmin):
     list_display = ('id', 'nome')
     search_fields = ('nome',)
 
+@admin.display(description='Prévia do texto')
+def resumo_texto(self, obj):
+    texto_limpo = BeautifulSoup(obj.texto, 'html.parser').get_text()
+    texto_limpo = texto_limpo.strip()
+    return texto_limpo[:80] + '...' if len(texto_limpo) > 80 else texto_limpo
 
 @admin.register(Artigo)
 class ArtigoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'id_fk_autor', 'id_fk_eixo', 'data_publicacao')
-    search_fields = ('id_fk_autor__nome',)
-    list_filter = ('id_fk_eixo', 'data_publicacao')
-
+    list_display = ('id', 'titulo', 'id_fk_autor', 'id_fk_eixo', 'nivel', 'resumo_texto', 'data_publicacao')
+    search_fields = ('titulo', 'id_fk_autor__nome', 'texto')
+    list_filter = ('id_fk_eixo', 'nivel', 'data_publicacao')
+    list_display_links = ('id', 'titulo')
     formfield_overrides = {
         db_models.TextField: {'widget': TinyMCE()},
     }
+    @admin.display(description='Prévia do texto')
+    def resumo_texto(self, obj):
+        texto_limpo = BeautifulSoup(obj.texto, 'html.parser').get_text()
+        texto_limpo = texto_limpo.strip()
+        return texto_limpo[:80] + '...' if len(texto_limpo) > 80 else texto_limpo
+
+
