78665f839678d970f7107deeca4e116c19ddc1c0
Marcos(django) <marcosvinicius2610@protonmail.com>
2026-07-01 17:44:40 -0300

    Aula 10

--- motorartigos/models.py
+++ motorartigos/models.py
@@ -14,6 +14,8 @@ class Autor(models.Model):
         return self.nome
     class Meta:
         db_table = 'autor'
+        verbose_name = 'Autor'
+        verbose_name_plural = 'Autores'
 
 
 class EixoTecnologia(models.Model):
@@ -43,16 +45,23 @@ class Artigo(models.Model):
         verbose_name='Título',
         default='Sem título'
     )
-    nivel = models.CharField(
-        max_length=20,
-        verbose_name='Nível',
-        default='Sem nível'
-    )
     foto = models.ImageField(upload_to="fotos/%Y/%m/%d/", blank=True)
-    publicada = models.BooleanField(default=True)
+    publicada = models.BooleanField(default=False)
+    TAG_NIVEL = [
+        ("B", "Básico"),
+        ("I", "Intermediário"),
+        ("A", "Avançado"),
+    ]
+    nivel = models.CharField(
+        max_length=1,
+        choices=TAG_NIVEL,
+        default='B',
+        verbose_name='Nível'
+    )
 
     def __str__(self):
-        return f"Artigo {self.id} - {self.data_publicação}"
+        return f"Artigo {self.id} - {self.data_publicacao}"
     
     class Meta:
         db_table = 'artigo'
+        ordering = ['-data_publicacao']
--- motorartigos/views.py
+++ motorartigos/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from motorartigos.models import Autor
+from motorartigos.models import Autor, Artigo
 
 def index(request):
     autores = Autor.objects.all()
+    artigos = Artigo.objects.filter(publicada=True)
    
-    return render(request,'motorartigos/index.html')
+    return render(request,'motorartigos/index.html', {'artigos': artigos})
 
 def artigo(request):
     return render(request,'motorartigos/artigo.html')
