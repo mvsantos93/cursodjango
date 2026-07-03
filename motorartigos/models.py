4b4367e955f4989cb22f30724f24adbdf743b6e1
mvsantos93 <marcosvinicius2610@protonmail.com>
2026-07-03 17:36:09 -0300

    Aula 12 completa

--- motorartigos/models.py
+++ motorartigos/models.py
@@ -1,11 +1,6 @@
 from django.db import models
 
-# Create your models here.
-# aqui vou criar minhas classses de entidade
-
 class Autor(models.Model):
-    #O atributo id é automático.
-    # chave primária: imutável, universal e única
     nome = models.CharField(max_length=100)
     biografia = models.TextField()
     email = models.EmailField()
--- motorartigos/views.py
+++ motorartigos/views.py
@@ -1,18 +1,36 @@
 from django.shortcuts import render
-from motorartigos.models import Autor, Artigo, EixoTecnologia
+from motorartigos.models import Artigo, EixoTecnologia
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
 
 def index(request):
-    autores = Autor.objects.all()
-    artigos = Artigo.objects.filter(publicada=True)
+    artigos_base = Artigo.objects.filter(publicada=True)
     eixos = EixoTecnologia.objects.all()
 
-    eixo = request.GET.get('eixo')
+    termo_busca = request.GET.get('busca')
+    eixo_id = request.GET.get('eixo')
 
-    if eixo: artigos = artigos.filter(id_fk_eixo_id=eixo)
-   
-    return render(request,'motorartigos/index.html', {
-        'artigos': artigos, 'artigos_recentes': Artigo.objects.filter(publicada=True)[:4], 'eixos': eixos, 'eixo_selecionado': eixo})
+    artigos_todos = artigos_base
+
+    if eixo_id: artigos_todos = artigos_todos.filter(id_fk_eixo__id=eixo_id)
+
+    if termo_busca: artigos_todos = artigos_todos.filter(
+        Q(titulo__icontains=termo_busca) | 
+        Q(texto__icontains=termo_busca) |
+         Q(id_fk_autor__nome__icontains=termo_busca) |
+        Q(id_fk_eixo__nome__icontains=termo_busca)
+        )
+    
+    artigos_recentes = artigos_base.order_by('-data_publicacao')[:4]
+
+    contexto = {
+        'artigos': artigos_todos,
+        'artigos_recentes': artigos_recentes,
+        'eixos': eixos,                           
+        'eixo_selecionado': eixo_id,               
+        'termo_busca': termo_busca                 
+    }
+    return render(request, 'motorartigos/index.html', contexto)
 
 def artigo(request, id):
     artigo = get_object_or_404(Artigo, id=id, publicada=True)
